20800b4758afecc85a97b139197728d9b49ed7af
K PRADEEP KUMAR <kpradeepkumar3005@gmail.com>
2026-07-01 00:37:42 +0530

    Updated files

---
+++ Place-x_Tasks/Day_1/d1.js
@@ -0,0 +1,21 @@
+function isPalindrome(x) {
+    if (x < 0) {
+        return false;
+    }
+
+    let original = x;
+    let reversed = 0;
+
+    while (x > 0) {
+        let digit = x % 10;
+        reversed = reversed * 10 + digit;
+        x = Math.floor(x / 10);
+    }
+
+    return original === reversed;
+}
+
+console.log(isPalindrome(121));   // true
+console.log(isPalindrome(-121));  // false
+console.log(isPalindrome(10));    // false
+
